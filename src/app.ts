9ddbacd48a3ccd49cd6b7300c1b5e2d39421eaf6
onishi taisei <taisei.developer@gmail.com>
2023-01-21 23:09:22 +0900

    DOM要素の操作

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -15,7 +18,32 @@ class ProjectInput {
     );
 
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      "#manday"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    // httpリクエストが送信されないようにする
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    // submitHandlerで使用しているthisはこのクラスを参照しているわけではないので
+    // bindしないとundefinedになる。bind(this)でクラスのthisを渡すことによってsubmitHandler側でthisを参照することができる
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
