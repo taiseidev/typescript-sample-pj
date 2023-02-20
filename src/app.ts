b7190c1aecbbd7ba169e99b9d43d72aea0abfba0
onishi taisei <taisei.developer@gmail.com>
2023-02-21 07:55:13 +0900

    プロジェクト項目の表示を変更

--- src/app.ts
+++ src/app.ts
@@ -165,6 +165,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   }
 }
 
+// ProjectItem Class
+// 各プロジェクトの項目
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+    this.configure();
+    this.renderContent();
+    {
+      this.element.querySelector("h2")!.textContent = this.project.title;
+      this.element.querySelector("h3")!.textContent =
+        this.project.manday.toString();
+      this.element.querySelector("p")!.textContent = this.project.description;
+    }
+  }
+
+  configure(): void {}
+
+  renderContent(): void {}
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -205,9 +227,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
     listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(listEl.id, prjItem);
     }
   }
 }
