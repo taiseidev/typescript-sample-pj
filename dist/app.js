b3dd4c8d3ceed5430628169627c9408a11856524
onishi taisei <taisei.developer@gmail.com>
2023-02-21 07:59:38 +0900

    getterを利用して日付表示を変更

--- dist/app.js
+++ dist/app.js
@@ -100,6 +100,14 @@ class Component {
     }
 }
 class ProjectItem extends Component {
+    get manday() {
+        if (this.project.manday < 20) {
+            return this.project.manday.toString() + "人日";
+        }
+        else {
+            return (this.project.manday / 20).toString() + "人月";
+        }
+    }
     constructor(hostId, project) {
         super("single-project", hostId, false, project.id);
         this.project = project;
@@ -107,8 +115,7 @@ class ProjectItem extends Component {
         this.renderContent();
         {
             this.element.querySelector("h2").textContent = this.project.title;
-            this.element.querySelector("h3").textContent =
-                this.project.manday.toString();
+            this.element.querySelector("h3").textContent = this.manday;
             this.element.querySelector("p").textContent = this.project.description;
         }
     }
--- src/app.ts
+++ src/app.ts
@@ -169,6 +169,15 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 // 各プロジェクトの項目
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+
+  get manday() {
+    if (this.project.manday < 20) {
+      return this.project.manday.toString() + "人日";
+    } else {
+      return (this.project.manday / 20).toString() + "人月";
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
     this.project = project;
@@ -176,8 +185,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderContent();
     {
       this.element.querySelector("h2")!.textContent = this.project.title;
-      this.element.querySelector("h3")!.textContent =
-        this.project.manday.toString();
+      this.element.querySelector("h3")!.textContent = this.manday;
       this.element.querySelector("p")!.textContent = this.project.description;
     }
   }
