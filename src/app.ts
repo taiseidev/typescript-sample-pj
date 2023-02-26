0fcf61cb6bb3207fbc50530ede9e9a2b5f78ec1d
onishi taisei <taisei.developer@gmail.com>
2023-02-27 01:12:31 +0900

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// プロジェクトの要素を表示する
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   // 親要素（<div id="app"></div>）
@@ -12,19 +13,22 @@ class ProjectInput {
 
     // this.templateElement.contentでtemplateタグの内側にある要素を取得
     // importNodeの第二引数にはbool値を指定する（deep cloneするかどうか）
-    // trueにすることによって最初の階層だけでなくtemplatem下の全ての要素を取得することができる
+    // trueにすることによって最初の階層だけでなくtemplate下の全ての要素を取得することができる
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    // user-inputというidを指定することによってcssを適用
+    this.element.id = "user-input";
     this.attach();
   }
 
-  // hostElementに要素を追加する
+  // hostElementに要素追加して画面を表示するメソッド
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
+// インスタンス化してフォームを表示する
 const prjInput = new ProjectInput();
